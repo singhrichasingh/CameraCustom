62a5c2755bea683f49d0cf2f76bc9b01c5488606
richa <richa@dyfolabs.com>
2020-08-17 17:17:18 +0530

    new changes

--- src/android/CameraCustom.java
+++ src/android/CameraCustom.java
@@ -24,82 +24,68 @@ import com.tuya.smart.android.network.TuyaSmartNetWork;
 import com.tuya.smart.home.sdk.TuyaHomeSdk;
 import com.tuya.smart.sdk.TuyaSdk;
 import com.tuya.smart.sdk.api.INeedLoginListener;
+import com.tuya.smart.android.user.api.IValidateCallback;
  import android.app.Activity;
 import android.widget.Toast;
 import org.apache.cordova.*;
 import android.util.Log;
 public class CameraCustom extends CordovaPlugin {
-     Context context=this.cordova.getActivity().getApplicationContext();
-        // Context context=this;
      private  final String TAG = "CameraCustom";
      public static CameraCustom instance = null;
        public  CordovaWebView cordovaWebView;
+     public Activity activity;
     public CordovaInterface cordovaInterface;
-    // public Application application=this.cordova.getActivity().getApplicationContext();
+   public Application application;
     private String appKey = "dt4jvymqr3kuvtg5ajcw";
     private String appSecret = "jkhtkxr98p8frqmwuffku3ht9qhy53ke";
-     public TuyaHomeSdk tuya=TuyaHomeSdk;
-     private String applicationId;
-    //public static Activity mainActivity=CameraCustom.instance.cordovaInterface.getActivity();
-    //     @Override
-    // public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-    //     super.initialize(cordova, webView);
-    //       Log.d(TAG, this+"Initializing MyCordovaPlugin");
-    //     TuyaHomeSdk.setDebugMode(true);
-    //     //    Application application=cordova.getActivity().getApplicationContext();
-    //     //    TuyaHomeSdk.init(application);
+     private String phoneCode = "+91";
+      private String phoneNumber = "9393939393";
+      public static final int MSG_SEND_VALIDATE_CODE_SUCCESS = 12;
+    @Override
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
 
-    //     instance = this;
-    //     cordovaWebView = webView;
-    //     cordovaInterface = cordova;
-    // }
-    // @Override
-    //  public void onCreate() {
-    //     super.onCreate();
-    //     context = this;
-        
-
-    //     TuyaHomeSdk.setDebugMode(true);
-    //     TuyaHomeSdk.init(this);
-    //     TuyaHomeSdk.setOnNeedLoginListener(new INeedLoginListener() {
-    //         @Override
-    //         public void onNeedLogin(Context context) {
-    //             Intent intent = new Intent(context, LoginActivity.class);
-    //             if (!(context instanceof Activity)) {
-    //                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-    //             }
-    //             startActivity(intent);
-    //         }
-    //     });
-    // }
+        instance = this;
+        cordovaWebView = webView;
+        cordovaInterface = cordova;
+    }
      @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-         this.applicationId = (String) BuildHelper.getBuildConfigValue(cordova.getActivity(), "APPLICATION_ID");
-        this.applicationId = preferences.getString("applicationId", this.applicationId);
-        // if(action.equals('initialize')){
-        //      Log.d(TAG, "nnnnnnnnnnnn heloop");
-        //      return true;
-        //  Log.d(TAG,this+"noe");
-        // //      return true;
-        // // }
-          if(action.equals("init")){
+         if(action.equals("init")){
              Log.d(TAG, this+"here");
-            // Context context=this.cordova.getActivity().getApplicationContext();
-             // Application application=tuya;
-         TuyaHomeSdk.init(this.applicationId,appKey,appSecret);
-           
-            return true;
+             Context context=this.cordova.getActivity().getApplicationContext();
+             this.application= (Application) context;
+         TuyaHomeSdk.init(application,appKey,appSecret);
+           TuyaHomeSdk.setOnNeedLoginListener(new INeedLoginListener(){
+               @Override
+                public void onNeedLogin(Context context) {
+                   Log.d(TAG,"whereeee"+ this); 
+                  TuyaHomeSdk.getUserInstance().getValidateCode(phoneCode,phoneNumber, mIValidateCallback);
+  String message = " :finished activity from native: ";
+          callbackContext.success(message);
+        //  TuyaHomeSdk.setDebugMode(true);
+                }
+    });
+             return true;
         }
         if (action.equals("search")) {
              Log.d(TAG, "Initializing heloop");
-            // Toast.makeText(mainActivity, "req timeout, please try again", Toast.LENGTH_LONG).show();
-           // String message = args.getString(0);
-           // this.echo(message, callbackContext);
             return true;
         }
         return false;
     }
 
 
+    private IValidateCallback mIValidateCallback = new IValidateCallback() {
+        @Override
+        public void onSuccess() {
+           // mHandler.sendEmptyMessage(MSG_SEND_VALIDATE_CODE_SUCCESS);
+        }
+
+        @Override
+        public void onError(String s, String s1) {
+          //  getValidateCodeFail(s, s1);
+        }
+    };
 
 }
